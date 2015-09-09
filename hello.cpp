708726190a51e5bd6cd1c02e62c39568cd4d9c9a
DevinBedard52 <bedardd@my.easternct.edu>
2015-09-09 14:44:11 -0400

    Updated the Hello World file
    Changed "Hello Class" to "Hello (person's name)"

--- hello.cpp
+++ hello.cpp
@@ -2,8 +2,12 @@
 using namespace std;
 
 int main () {
+
+  cout << "What's your name?" << endl;
+
+  cin >> name >> endl;
   
-  cout << "Hello Class!\n\n";
+  cout << "Hello " << name << "!" << endl;
 
   return 0;
 }
